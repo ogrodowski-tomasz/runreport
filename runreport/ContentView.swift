fd9190c0df26a945b32990e34f23a149c178984e
Tomasz Ogrodowski <tom.ogrodowski@gmail.com>
2025-11-13 17:42:50 +0100

    Initial Commit

---
+++ runreport/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//
+// ContentView.swift
+// runreport
+//
+// Created by Tomasz Ogrodowski on 13/11/2025
+// Copyright © 2025 Tomasz Ogrodowski. All rights reserved.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ runreport/runreportApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// runreportApp.swift
+// runreport
+//
+// Created by Tomasz Ogrodowski on 13/11/2025
+// Copyright © 2025 Tomasz Ogrodowski. All rights reserved.
+//
+        
+
+import SwiftUI
+
+@main
+struct runreportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
